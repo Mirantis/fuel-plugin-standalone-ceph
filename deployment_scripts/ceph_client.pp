9841df2d1a112081ff0b2a8eb7e29a54c3fe55c0
Ukov Dmitry <dukov@mirantis.com>
2015-10-20 13:20:37 +0300

    Added rados gw on ceph mons
    Change-Id: Ie0f828a7304ff867562daf986e88ce2a23cab805

---
+++ deployment_scripts/ceph_client.pp
@@ -0,0 +1,48 @@
+notice('MODULAR: ceph_client.pp')
+
+$ceph_primary_monitor_node = hiera('ceph_primary_monitor_node')
+$primary_mons              = keys($ceph_primary_monitor_node)
+$primary_mon               = $ceph_primary_monitor_node[$primary_mons[0]]['name']
+
+include ceph::ssh
+include ceph::params
+
+Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+       cwd  => '/root',
+}
+
+exec {'ceph-deploy config pull':
+  command   => "ceph-deploy --overwrite-conf config pull ${primary_mon}",
+  cwd       => '/etc/ceph',
+  creates   => '/etc/ceph/ceph.conf',
+  tries     => 5,
+  try_sleep => 2,
+}
+
+file {'/root/ceph.conf':
+  ensure => link,
+  target => '/etc/ceph/ceph.conf',
+}
+
+exec {'ceph-deploy gatherkeys remote':
+  command   => "ceph-deploy gatherkeys ${primary_mon}",
+  creates   => ['/root/ceph.bootstrap-mds.keyring',
+                '/root/ceph.bootstrap-osd.keyring',
+                '/root/ceph.client.admin.keyring',
+                '/root/ceph.mon.keyring',],
+  tries     => 5,
+  try_sleep => 2,
+}
+
+file {'/etc/ceph/ceph.client.admin.keyring':
+  ensure => file,
+  source => '/root/ceph.client.admin.keyring',
+  mode   => '0600',
+  owner  => 'root',
+  group  => 'root',
+}
+
+Exec['ceph-deploy config pull'] ->
+    File['/root/ceph.conf'] ->
+      Exec['ceph-deploy gatherkeys remote'] ->
+        File['/etc/ceph/ceph.client.admin.keyring']
---
+++ deployment_scripts/openstack-haproxy-radosgw.pp
@@ -0,0 +1,38 @@
+notice('MODULAR: openstack-haproxy-radosgw.pp')
+
+$network_metadata = hiera_hash('network_metadata')
+$storage_hash     = hiera_hash('storage', {})
+$public_ssl_hash  = hiera('public_ssl')
+
+
+if !($storage_hash['images_ceph'] and $storage_hash['objects_ceph']) and !$storage_hash['images_vcenter'] {
+  $use_swift = true
+} else {
+  $use_swift = false
+}
+if !($use_swift) and ($storage_hash['objects_ceph']) {
+  $use_radosgw = true
+} else {
+  $use_radosgw = false
+}
+
+if $use_radosgw {
+  $rgw_address_map     = get_node_to_ipaddr_map_by_network_role(hiera('ceph_rgw_nodes'), 'ceph/radosgw')
+  $server_names        = hiera_array('radosgw_server_names', keys($rgw_address_map))
+  $ipaddresses         = hiera_array('radosgw_ipaddresses', values($rgw_address_map))
+  if $network_metadata {
+    $public_virtual_ip = $network_metadata['vips']['public_rados_ep']['ipaddr']
+    $internal_virtual_ip = $network_metadata['vips']['rados_ep']['ipaddr']
+  } else {
+    fail("No network metadata")
+  }
+
+  # configure radosgw ha proxy
+  class { '::openstack::ha::radosgw':
+    internal_virtual_ip => $internal_virtual_ip,
+    ipaddresses         => $ipaddresses,
+    public_virtual_ip   => $public_virtual_ip,
+    server_names        => $server_names,
+    public_ssl          => $public_ssl_hash['services'],
+  }
+}

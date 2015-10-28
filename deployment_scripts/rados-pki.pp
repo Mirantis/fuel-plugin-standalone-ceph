d6b800ab595a7319188623a967e973ef90a49d94
Ukov Dmitry <dukov@mirantis.com>
2015-10-28 18:53:08 +0300

    Added Ceph keys generation for keystone revocation
    Change-Id: If9848be5669e01ce71fcc1398baf8a84a0711c87

---
+++ deployment_scripts/rados-pki.pp
@@ -0,0 +1,26 @@
+notice('MODULAR: ceph/mon.pp')
+
+include ::ceph::params
+$rgw_nss_db_path = '/etc/ceph/nss'
+$rgw_id          = 'radosgw.gateway'
+$rgw_user    = $::ceph::params::user_httpd
+
+Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+  cwd  => '/root',
+}
+
+
+ceph_conf {
+  "client.${rgw_id}/nss db path": value => $rgw_nss_db_path;
+}
+
+# This creates the signing certs used by radosgw to check cert revocation
+#   status from keystone
+exec {'create nss db signing certs':
+  command => "openssl x509 -in /var/lib/astute/keystone/ssl/certs/ca.pem -pubkey | \
+    certutil -d ${rgw_nss_db_path} -A -n ca -t 'TCu,Cu,Tuw' && \
+    openssl x509 -in /var/lib/astute/keystone/ssl/certs/signing_cert.pem -pubkey | \
+    certutil -A -d ${rgw_nss_db_path} -n signing_cert -t 'P,P,P'",
+} ->
+exec {"chown -R ${rgw_user} ${rgw_nss_db_path}":}
+
